012af5c97f6b888871e9531d948a90f5406a0a76
Kevin chinedu <kelvinchinedu97@gmail.com>
2026-08-14 22:20:00 +0100

    Update twrp_X6711.mk

--- twrp_X6711.mk
+++ twrp_X6711.mk
@@ -1,18 +1,16 @@
 #
 # Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-#Inherit emulated_storage properties
+# Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
-# Inherit userspace reboot properties
-$(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
